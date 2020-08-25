3e5764e3bb523422432f27da18bdfc899f6a669d
Rocio Gomez <rociogomezbardon@gmail.com>
2020-08-25 13:01:13 +0300

    cleaning media folder

--- web_sudoku_solver/views.py
+++ web_sudoku_solver/views.py
@@ -49,6 +49,16 @@ def clear_media_folder():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
+    folder= os.path.join(settings.BASE_DIR,'staticfiles','media')
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def checkRotationAndSave(f):
     imageRotated = get_rotated_image(f)
