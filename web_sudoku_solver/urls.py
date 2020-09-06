4c80c40d7a12dcbbde5086a176b094ed18c81246
Rocio Gomez <rociogomezbardon@gmail.com>
2020-09-06 20:14:57 +0300

    modifications to allow media to be shown

--- web_sudoku_solver/urls.py
+++ web_sudoku_solver/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('admin/', admin.site.urls),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ]
