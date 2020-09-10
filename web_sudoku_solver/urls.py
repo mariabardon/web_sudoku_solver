131acbe547e6a7f8355b2d060c2bed979a5a84c8
Rocio Gomez <rociogomezbardon@gmail.com>
2020-09-10 15:39:31 +0300

    testing if new changes will display uploaded images

--- web_sudoku_solver/urls.py
+++ web_sudoku_solver/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings # new
+from django.conf.urls.static import static # new
+
 from . import views
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('admin/', admin.site.urls),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
